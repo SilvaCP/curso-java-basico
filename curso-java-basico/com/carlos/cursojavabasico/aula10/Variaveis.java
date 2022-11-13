4571487e545dd4dbd2b6ef809de33aaa0940cc0f
Carlos <pereiracarlos1047@gmail.com>
2022-11-13 15:29:50 -0300

    Upload inicial.

---
+++ curso-java-basico/com/carlos/cursojavabasico/aula10/Variaveis.java
@@ -0,0 +1,14 @@
+package com.carlos.cursojavabasico.aula10;
+
+public class Variaveis {
+	public static void main(String[] args) {
+		int idade = 27;
+		String nome = "Carlos";
+		String nomeDoMeuCachorro = "Totó";
+		int ano = 2014;
+		
+		System.out.println("Ano: " + ano);
+		System.out.print("Nome:" + nome + " Idade:" + idade + " Nome do meu cachorro:" + nomeDoMeuCachorro);
+	}
+
+}
