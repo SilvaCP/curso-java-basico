d07758163ae40bd0b0753dde7e1e365b97fcbd90
Carlos <pereiracarlos1047@gmail.com>
2022-11-14 10:59:03 -0300

    Atualização da aula.

---
+++ curso-java-basico/com/carlos/cursojavabasico/aula13/OperadoresAritmeticos.java
@@ -0,0 +1,55 @@
+package com.carlos.cursojavabasico.aula13;
+
+public class OperadoresAritmeticos {
+	public static void main(String[] args) {
+		
+//		int resultado = 1 + 2;
+//		System.out.println(resultado);
+//		
+//		resultado+= -1;
+//		System.out.println(resultado);
+//		
+//		resultado = resultado * 2;
+//		System.out.println(resultado);
+//		
+//		resultado = resultado / 2;
+//		System.out.println(resultado);
+//		
+//		resultado+= + 8;
+//		System.out.println(resultado);
+//		
+//		resultado = resultado % 7;
+//		System.out.println(resultado);
+		
+//		String primeiroNome = "Esta é";
+//		String segundoNome = " uma String concatenada.";
+//		String terceiroNome = primeiroNome + segundoNome;
+//		System.out.println(terceiroNome);
+		
+		int resultado2 = 3;
+		System.out.println(resultado2); // 3
+		resultado2 += 1;
+		System.out.println(resultado2); // 4
+		resultado2++;
+		System.out.println(resultado2); // 5
+		System.out.println(++resultado2); // 6
+		
+		resultado2--;
+		System.out.println(resultado2); // 5
+		System.out.println(resultado2--); // 5
+		System.out.println(resultado2);   // 4
+		System.out.println(--resultado2); // 3
+		System.out.println(resultado2++); // 3
+		System.out.println(resultado2); // 4
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ curso-java-basico/com/carlos/cursojavabasico/aula13/OperadoresLogicos.java
@@ -0,0 +1,13 @@
+package com.carlos.cursojavabasico.aula13;
+
+public class OperadoresLogicos {
+
+	public static void main(String[] args) {
+		int valor1 = 1;
+		int valos2 = 2;
+		boolean resultado1 = (valor1 == 1) && (valos2 == 2);
+		System.out.println("valor1 é 1 OR valor2 é 2 - resultado: " + resultado1);
+
+	}
+
+}
