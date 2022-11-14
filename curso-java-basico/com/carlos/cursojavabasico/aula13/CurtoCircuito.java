90db918e8606ca77a886a61006027b537d318575
Carlos <pereiracarlos1047@gmail.com>
2022-11-14 12:07:06 -0300

    Fiz alteração no código.

---
+++ curso-java-basico/com/carlos/cursojavabasico/aula13/CurtoCircuito.java
@@ -0,0 +1,18 @@
+package com.carlos.cursojavabasico.aula13;
+
+public class CurtoCircuito {
+	public static void main(String[] args) {
+//		boolean verdadeiro = true;
+//		boolean falso = false;
+//		boolean resultado1 = falso & 
+//				verdadeiro; // Olha a segunda expressão.
+//		boolean resultado2 = falso && 
+//				verdadeiro; // O programa, nem olha a segunda expressão.
+//		System.out.println(resultado1);
+//		System.out.println(resultado2);
+		
+		int resultado = 1 + 1 - 1 + 1 * 1 / 1;
+		System.out.println(resultado);
+	}	
+
+}
--- curso-java-basico/com/carlos/cursojavabasico/aula13/OperadoresAritmeticos.java
+++ curso-java-basico/com/carlos/cursojavabasico/aula13/OperadoresAritmeticos.java
@@ -26,21 +26,36 @@ public class OperadoresAritmeticos {
 //		String terceiroNome = primeiroNome + segundoNome;
 //		System.out.println(terceiroNome);
 		
-		int resultado2 = 3;
-		System.out.println(resultado2); // 3
-		resultado2 += 1;
-		System.out.println(resultado2); // 4
-		resultado2++;
-		System.out.println(resultado2); // 5
-		System.out.println(++resultado2); // 6
+//		int resultado2 = 3;
+//		System.out.println(resultado2); // 3
+//		resultado2 += 1;
+//		System.out.println(resultado2); // 4
+//		resultado2++;
+//		System.out.println(resultado2); // 5
+//		System.out.println(++resultado2); // 6
+//		
+//		resultado2--;
+//		System.out.println(resultado2); // 5
+//		System.out.println(resultado2--); // 5
+//		System.out.println(resultado2);   // 4
+//		System.out.println(--resultado2); // 3
+//		System.out.println(resultado2++); // 3
+//		System.out.println(resultado2); // 4
 		
-		resultado2--;
-		System.out.println(resultado2); // 5
-		System.out.println(resultado2--); // 5
-		System.out.println(resultado2);   // 4
-		System.out.println(--resultado2); // 3
-		System.out.println(resultado2++); // 3
-		System.out.println(resultado2); // 4
+		int resultado = + 1;
+		System.out.println(resultado);
+		resultado--;
+		System.out.println(resultado);
+		resultado++;
+		System.out.println(resultado); // 1
+		resultado = -resultado;
+		System.out.println(resultado); // -1
+		boolean success = false;
+		System.out.println(success); // false
+		System.out.println(!success); // true
+		
+		
+		
 		
 		
 		
