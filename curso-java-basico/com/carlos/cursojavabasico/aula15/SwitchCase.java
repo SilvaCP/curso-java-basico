5c3ed88531ea4f11ca2dd46bd10a74b1721af69e
Carlos <pereiracarlos1047@gmail.com>
2022-11-19 22:49:40 -0300

    Commit

--- curso-java-basico/com/carlos/cursojavabasico/aula15/SwitchCase.java
+++ curso-java-basico/com/carlos/cursojavabasico/aula15/SwitchCase.java
@@ -28,7 +28,7 @@ public class SwitchCase {
 //		}
 		
 		switch(diaSemana) {
-		case 1: System.out.println("Domingo "); break; // Senão tiver break, ele sai excecutando até encontar um break.
+		case 1: System.out.println("Domingo "); break; // Senão não tiver break, ele sai excecutando até encontar um break.
 		case 2: System.out.println("Segunda-feira "); break;
 		case 3: System.out.println("Terça-feira "); break;
 		case 4: System.out.println("Quarta-feira "); break;
