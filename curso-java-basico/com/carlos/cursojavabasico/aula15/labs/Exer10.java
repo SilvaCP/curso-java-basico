0951f3b13b32adce16b3b984676a0ab889758d57
Carlos <pereiracarlos1047@gmail.com>
2022-11-21 23:42:18 -0300

    Atualizei o Exer10

--- curso-java-basico/com/carlos/cursojavabasico/aula15/labs/Exer10.java
+++ curso-java-basico/com/carlos/cursojavabasico/aula15/labs/Exer10.java
@@ -9,24 +9,38 @@ import java.util.Scanner;
  * Imprima a mensagem "Bom dia!", "Boa tarde!", "Boa noite!" ou 
  * "Valor inválido!", conforme o caso. 
  * */
+
+	//Usando o if utiliza o método equalsIgnoreCase.
+	// Usando o Switch Case precisa utilizar letra minúscula e maiúscula.
 public class Exer10 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.println("Em que turno você estuda?");
 		System.out.println("Digite M - para matutino, V - para vespertino, N - para noturno.");
-		// Usando o if utiliza o método ignoreCase.
-		// Usando o Switch Case precisa utilizar letra minúscula e maiúscula.
+		
 		String turno = scan.next();
 		
-		switch(turno) {
-		case "m":
-			case "M": System.out.println("Bom dia!");break;
-			case "v":
-			case "V": System.out.println("Bom tarde!");break;
-			case "n":
-			case "N": System.out.println("Bom noite!");break;
-			default: System.out.println("Valor inválido!");
+//		switch(turno) {
+//		case "m":
+//			case "M": System.out.println("Bom dia!");break;
+//			case "v":
+//			case "V": System.out.println("Bom tarde!");break;
+//			case "n":
+//			case "N": System.out.println("Bom noite!");break;
+//			default: System.out.println("Valor inválido!");
+//		}
+		
+		// Abaixo o mesmo programa usando if/else. com o método, equalsIgnoreCase
+		
+		if(turno.equalsIgnoreCase("m")) {
+			System.out.println("Turno Matutino. Bom dia!");
+		} else if(turno.equalsIgnoreCase("v")) {
+			System.out.println("Turno Vespertino. Boa tarde!");
+		} else if(turno.equalsIgnoreCase("n")) {
+			System.out.println("Turno Noturno. Boa noite!");
+		} else {
+			System.out.println("Valor inválido!");
 		}
 		
 		scan.close();
